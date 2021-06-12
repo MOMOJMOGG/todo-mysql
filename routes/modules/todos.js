55b7107121ddc33accfc25db63dbe14b54734e44
MOMOJMOGG <momomacaca@gmail.com>
2021-06-12 22:47:12 +0800

    feat: modify new routes

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -9,9 +9,31 @@ router.get('/new', (req, res) => {
   return res.render('new')
 })
 
-router.post('/new', (req, res) => {
-  const userId = req.user.id
-  const name = req.body.name
+router.post('/new', async (req, res) => {
+  try {
+    const userId = req.user.id
+    const name = req.body.name
+    const errors = []
+
+    if (!name) {
+      errors.push({ message: '欄位不得為空白!' })
+    }
+    if (name.length > 30) {
+      errors.push({ message: '字數不得超過 30。' })
+    }
+    if (errors.length) {
+      return res.render('new', { errors, name })
+    }
+
+    const todo = await Todo.create({ name, UserId: userId })
+    console.log(todo.id, todo.UserId)
+    req.flash('success_msg', '待辦清單新增成功, 可繼續更新或回到首頁!')
+    return res.redirect('/')
+  } catch (err) {
+    return res.status(422).json(err)
+  }
+
+
   // const todo = new Todo({ name })
   // return todo.save()
   //   .then(() => res.redirect('/'))
