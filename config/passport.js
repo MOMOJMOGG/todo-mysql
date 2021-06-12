73d34724eb775d2fe2ef9c17637f0b2c45066a21
MOMOJMOGG <momomacaca@gmail.com>
2021-06-12 23:48:12 +0800

    feat: add passport-facebook

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
-// const FacebookStrategy = require('passport-facebook').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
@@ -28,30 +28,30 @@ module.exports = app => {
   }))
 
   // passport Facebook 登入策略
-  // passport.use(new FacebookStrategy({
-  //   clientID: process.env.FACEBOOK_APP_ID,
-  //   clientSecret: process.env.FACEBOOK_APP_SECRET,
-  //   callbackURL: process.env.FACEBOOK_APP_CALLBACK_URL,
-  //   profileFields: ['email', 'displayName']
-  // }, async (accessToken, refreshToken, profile, done) => {
-  //   try {
-  //     const { name, email } = profile._json
-  //     let user = await User.findOne({ where: { email } })
-  //     if (user) {
-  //       return done(null, user)
-  //     }
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: process.env.FACEBOOK_APP_CALLBACK_URL,
+    profileFields: ['email', 'displayName']
+  }, async (accessToken, refreshToken, profile, done) => {
+    try {
+      const { name, email } = profile._json
+      let user = await User.findOne({ where: { email } })
+      if (user) {
+        return done(null, user)
+      }
 
-  //     const randomPassword = Math.random().toString(36).slice(-8) // 隨機產生 0~1小數 | 36: 26英文字母 + 10 數字總數 | 擷取最後8碼
-  //     const salt = await bcrypt.genSalt(10)
-  //     const hash = await bcrypt.hash(randomPassword, salt)
-  //     user = await User.create({ name, email, password: hash })
-  //     if (user) {
-  //       return done(null, user)
-  //     }
-  //   } catch (err) {
-  //     return done(err, false)
-  //   }
-  // }))
+      const randomPassword = Math.random().toString(36).slice(-8) // 隨機產生 0~1小數 | 36: 26英文字母 + 10 數字總數 | 擷取最後8碼
+      const salt = await bcrypt.genSalt(10)
+      const hash = await bcrypt.hash(randomPassword, salt)
+      user = await User.create({ name, email, password: hash })
+      if (user) {
+        return done(null, user)
+      }
+    } catch (err) {
+      return done(err, false)
+    }
+  }))
 
   passport.serializeUser((user, done) => {
     done(null, user.id)
