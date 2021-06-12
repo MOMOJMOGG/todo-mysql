ca4f0b50de20585b30161d82f3483488fc2c4877
MOMOJMOGG <momomacaca@gmail.com>
2021-06-12 21:15:32 +0800

    feat: add sequalize passport

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const usePassport = require('./config/passport')
+
 
 const app = express()
 const PORT = 3000
@@ -12,29 +15,81 @@ const User = db.User
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  res.render('index')
+usePassport(app)
+
+const passport = require('passport')
+
+app.get('/', async (req, res) => {
+  try {
+    const todos = await Todo.findAll({
+      raw: true,
+      nest: true
+    })
+    return res.render('index', { todos: todos })
+  } catch (err) {
+    return res.status(422).json(err)
+  }
+})
+
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const id = req.params.id
+    const todo = await Todo.findByPk(id)
+    return res.render('detail', { todo: todo.toJSON() })
+  } catch (err) {
+    return res.status(422).json(err)
+  }
 })
 
 app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
 })
 
-app.post('/users/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  User.create({ name, email, password })
-    .then(user => res.redirect('/'))
+app.post('/users/register', async (req, res) => {
+  try {
+    const { name, email, password, confirmPassword } = req.body
+    let user = await User.findOne({ where: { email } })
+    if (user) {
+      console.log(user)
+      console.log('User already exists')
+      return res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+    user = await User.create({
+      name,
+      email,
+      password: hash
+    })
+    console.log(user)
+    return res.redirect('/')
+  } catch (err) {
+    return res.status(422).json(err)
+  }
 })
 
 app.get('/users/logout', (req, res) => {
---
+++ config/passport.js
@@ -0,0 +1,39 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const User = db.User
+module.exports = app => {
+  app.use(passport.initialize())
+  app.use(passport.session())
+  passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+    try {
+      const user = await User.findOne({ where: { email } })
+
+      if (!user) {
+        return done(null, false, { message: 'That email is not registered!' })
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password)
+      if (!isMatch) {
+        return done(null, false, { message: 'Email or Password incorrect.' })
+      }
+
+      return done(null, user)
+    } catch (err) {
+      return console.warn(err)
+    }
+  }))
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+  passport.deserializeUser(async (id, done) => {
+    try {
+      let user = await User.findByPk(id)
+      user = user.toJSON()
+      return done(null, user)
+    } catch (err) {
+      return console.warn(err)
+    }
+  })
+}
